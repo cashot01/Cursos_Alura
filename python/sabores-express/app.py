ecccf453f03479c785bd842f0b849e7822400d29
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-03-28 12:07:22 -0300

    app curso alura python

--- python/sabores-express/app.py
+++ python/sabores-express/app.py
@@ -1,4 +1,7 @@
-print("Sabor Express\n")
+print("""
+𝕊𝕒𝕓𝕠𝕣 𝔼𝕩𝕡𝕣𝕖𝕤𝕤
+""")
+# para pegar essas letras diferentes no f symbols
 
 print("1. Cadastrar Restaurante")
 print("2. Listar Restaurante")
@@ -6,4 +9,4 @@ print("3. Ativar Restaurante")
 print("4. Sair\n")
 
 opcao_escolhida = input("Escolha uma opção: ")
-print("Você escolheu a opção", opcao_escolhida)
+print(f"Você escolheu a opção {opcao_escolhida}")
