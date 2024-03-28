fbb4e44e3d8efec5c16fe579404f50ab215a3b98
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-03-28 12:43:48 -0300

    adicionando novas funções

--- python/sabores-express/app.py
+++ python/sabores-express/app.py
@@ -1,27 +1,38 @@
 import os
 os.system("cls")
 
-print("""
-    𝕊𝕒𝕓𝕠𝕣 𝔼𝕩𝕡𝕣𝕖𝕤𝕤
+def exibir_nome_programa():
+    print(""" 𝕊𝕒𝕓𝕠𝕣 𝔼𝕩𝕡𝕣𝕖𝕤𝕤
 """)
 # para pegar essas letras diferentes no f symbols
-
-print("1. Cadastrar Restaurante")
-print("2. Listar Restaurante")
-print("3. Ativar Restaurante")
-print("4. Sair\n")
-
-opcao_escolhida = int(input("Escolha uma opção: "))
-
+    
 def finalizar_app(): # def - função em python
     os.system("cls")
     print("finalizando o app")
 
-if opcao_escolhida == 1:
-    print("Cadastrando Restaurante")
-elif opcao_escolhida == 2:
-    print("Listar Restaurante")
-elif opcao_escolhida == 3:
-    print("Ativar Restaurante")
-else:
-    finalizar_app()
+def exibir_opcoes():
+    print("1. Cadastrar Restaurante")
+    print("2. Listar Restaurante")
+    print("3. Ativar Restaurante")
+    print("4. Sair\n")
+
+def escolher_opcao():
+    opcao_escolhida = int(input("Escolha uma opção: "))
+
+    if opcao_escolhida == 1:
+        print("Cadastrando Restaurante")
+    elif opcao_escolhida == 2:
+        print("Listar Restaurante")
+    elif opcao_escolhida == 3:
+        print("Ativar Restaurante")
+    else:
+        finalizar_app()
+
+
+def main():
+    exibir_nome_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == "__main__":
+    main()
