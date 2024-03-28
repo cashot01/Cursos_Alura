55114dbb8beee69aaecfb07be7e7491be59225dd
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-03-28 13:50:10 -0300

    ex1 python alura

---
+++ python/exercicios/ex1.py
@@ -0,0 +1,8 @@
+"""
+1 - Solicite ao usuário que insira um número e, em seguida, use uma estrutura if else para determinar se o número é par ou ímpar.
+"""
+num = int(input("nº: "))
+if num % 2 == 0:
+    print("par")
+else:
+    print("impar")
