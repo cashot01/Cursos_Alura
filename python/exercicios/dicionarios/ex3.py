963797d6444c04af00312819d94b96730f5568ea
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-06-10 12:23:33 -0300

    dicionarios ex3

---
+++ python/exercicios/dicionarios/ex3.py
@@ -0,0 +1,11 @@
+import os
+os.system("cls")
+"""
+3 - Crie um dicionário utilizando para representar números e seus quadrados de 1 a 5.
+"""
+# Criando o dicionário para representar números e seus quadrados de 1 a 5
+numeros_quadrados = {numero: numero ** 2 for numero in range(1, 6)}
+
+# Exibindo o dicionário
+print(numeros_quadrados)
+
