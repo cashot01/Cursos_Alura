5589493cd1a69c55c4348d83e7845bb203019c80
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-06-10 10:10:53 -0300

    ex 10.2 python

---
+++ python/exercicios/ex10_2.py
@@ -0,0 +1,8 @@
+import os
+os.system("cls")
+"""
+2 - Crie uma lista e utilize um loop for para percorrer todos os elementos da lista.
+"""
+frutas = ["uva", "banana", "maçã"]
+for fruta in frutas:
+    print(f"{fruta}")
