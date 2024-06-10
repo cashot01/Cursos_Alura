5f0e48fdbc922743558d56a1a3bf4fe09c49d08e
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-06-10 12:03:07 -0300

    dicionarios ex1

---
+++ python/exercicios/dicionarios/ex1.py
@@ -0,0 +1,9 @@
+import os
+os.system("cls")
+"""
+1 - Crie um dicionário representando informações sobre uma pessoa, como nome, idade e cidade.
+"""
+
+pessoa = [{"nome": "Cauan", "idade": 18, "cidade": "São Paulo"}]
+
+print(pessoa)
