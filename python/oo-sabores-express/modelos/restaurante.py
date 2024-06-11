19e29e402d65e5a13772e3eca5d05e51295d193d
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-06-11 12:17:17 -0300

    dir e vars python

--- python/oo-sabores-express/modelos/restaurante.py
+++ python/oo-sabores-express/modelos/restaurante.py
@@ -1,12 +1,31 @@
 # class = classe 
 class Restaurante:
+    # atributos da classe
     nome = ""
     categoria = ""
     ativo = False
 
 restaurante_praca = Restaurante()
+restaurante_praca.nome = "Praça"
+restaurante_praca.categoria = "Gourmet"
+
+
+
+
 restaurante_pizza = Restaurante()
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
-print(restaurantes)
+print(dir(restaurante_praca))
+
+"""
+ objeto dir() já possui uma série de informações, como __classe__. Sempre que temos esse __ (underline, underline), significa que é um método especial que toda classe em Python possui.
+
+ exemplo: ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'ativo', 'categoria', 'nome']
+
+  o dir() mostrará tudo: os atributos, métodos e propriedades de um objeto. 
+
+  Quando o substituímos por vars(), queremos um dicionário desses atributos e métodos.
+
+  exemplo: {'nome': 'Praça', 'categoria': 'Gourmet'}
+"""
