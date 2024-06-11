071fe4037e330f1a6af86f14c7ef351db8ebac7e
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-06-11 12:23:14 -0300

    ex classe musica

---
+++ python/oo-sabores-express/exercicios/classeMusica.py
@@ -0,0 +1,12 @@
+
+class Musica:
+    nome = ""
+    artista = ""
+    duracao = int
+
+musica1 = Musica()
+musica1.nome = "Superman"
+musica1.artista = "Emminem"
+musica1.duracao = 245
+
+print(vars(musica1))
