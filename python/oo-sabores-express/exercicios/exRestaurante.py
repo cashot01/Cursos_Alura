ee04b1739e5eb5e46a7337bbf140dfcff5cf0e53
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-06-11 14:27:22 -0300

    ex restaurante

--- python/oo-sabores-express/exercicios/exRestaurante.py
+++ python/oo-sabores-express/exercicios/exRestaurante.py
@@ -9,7 +9,15 @@ class Restaurante:
 
 restaurante_praca = Restaurante()
 restaurante_praca.nome = "Praça"
-restaurante_praca.categoria = "Gourmet"
+restaurante_praca.categoria = "Italiana"
+
+if restaurante_praca.ativo:
+    print(f"Restaurante está ativo")
+else:
+    print(f"Restaurante está desativado")
+
+categoria = restaurante_praca.categoria
+restaurante_praca.nome = "Bistrô"
 
 
 
@@ -17,7 +25,10 @@ restaurante_praca.categoria = "Gourmet"
 restaurante_pizza = Restaurante()
 restaurante_pizza.nome = "Pizza Place"
 restaurante_pizza.categoria = "Fast Food"
+restaurante_pizza.ativo = True
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
 print(vars(restaurante_praca))
+print()
+print(vars(restaurante_pizza))
