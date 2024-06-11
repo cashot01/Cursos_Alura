ea1bbaf9f212aba6bc72ddf89d135051e2efd6db
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-06-11 11:26:41 -0300

    docstrings nas funções

---
+++ python/exercicios/docstrings/docstrings.py
@@ -0,0 +1,66 @@
+
+def exibir_nome_do_programa():
+    ''' Exibe o nome estilizado do programa na tela '''
+    
+def exibir_opcoes():
+    ''' Exibe as opções disponíveis no menu principal '''
+
+def finalizar_app():
+    ''' Exibe mensagem de finalização do aplicativo '''
+
+def voltar_ao_menu_principal():
+    ''' Solicita uma tecla para voltar ao menu principal 
+    
+    Outputs:
+    - Retorna ao menu principal
+    '''
+
+def opcao_invalida():
+    ''' Exibe mensagem de opção inválida e retorna ao menu principal 
+    
+    Outputs:
+    - Retorna ao menu principal
+    '''
+
+def exibir_subtitulo(texto):
+    ''' Exibe um subtítulo estilizado na tela 
+    
+    Inputs:
+    - texto: str - O texto do subtítulo
+    '''
+
+def cadastrar_novo_restaurante():
+    ''' Essa função é responsável por cadastrar um novo restaurante 
+    
+    Inputs:
+    - Nome do restaurante
+    - Categoria
+
+    Outputs:
+    - Adiciona um novo restaurante a lista de restaurantes
+
+    '''
+
+def listar_restaurantes():
+    ''' Lista os restaurantes presentes na lista 
+    
+    Outputs:
+    - Exibe a lista de restaurantes na tela
+    '''
+
+def alternar_estado_restaurante():
+    ''' Altera o estado ativo/desativado de um restaurante 
+    
+    Outputs:
+    - Exibe mensagem indicando o sucesso da operação
+    '''
+
+def escolher_opcao():
+    ''' Solicita e executa a opção escolhida pelo usuário 
+    
+    Outputs:
+    - Executa a opção escolhida pelo usuário
+    '''
+
+def main():
+    ''' Função principal que inicia o programa '''
