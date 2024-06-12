3a3c32110bc3bc63cea8f8e6628ed2608cfe2dea
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-06-12 09:48:39 -0300

    metodos especiais

--- python/oo-sabores-express/modelos/restaurante.py
+++ python/oo-sabores-express/modelos/restaurante.py
@@ -3,24 +3,31 @@ os.system("cls")
 # class = classe 
 class Restaurante:
     # atributos da classe
-    nome = ""
-    categoria = ""
-    ativo = False
+    def __init__(self, nome, categoria): # construtor 
+      # self tbm pode ser this (do java)
+      self.nome = nome
+      self.categoria = categoria
+      self.ativo = False
+    
+    # metodo especiais
+    def __str__(self):
+      """
+      O método __str__ é um método especial que pega o objeto e define que, se precisarmos mostrar esse objeto em formato de texto, mostraremos determinada informação.
+      Então, em vez de mostrar a representação do endereço de memória, ele mostrará o que decidirmos.
+      """
+      return f"{self.nome} | {self.categoria} "
+      
+    
 
-restaurante_praca = Restaurante()
-restaurante_praca.nome = "Praça"
-restaurante_praca.categoria = "Gourmet"
-
-
-
-
-restaurante_pizza = Restaurante()
-restaurante_pizza.nome = "Pizza Place"
-restaurante_pizza.categoria = "Fast Food"
+restaurante_praca = Restaurante("Praça", "Gourmet")
+restaurante_pizza = Restaurante("PIzza Express", "Italiana")
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
-print(vars(restaurante_praca))
+print(restaurante_praca)
+print(restaurante_pizza)
+
+# print(vars(restaurante_praca))
 
 """
  objeto dir() já possui uma série de informações, como __classe__. Sempre que temos esse __ (underline, underline), significa que é um método especial que toda classe em Python possui.
