b30528efa656cc5d14730c404c9c78337202a446
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-06-12 10:12:11 -0300

    melhorando o ex musica com metodos especiais e proprios

--- python/oo-sabores-express/exercicios/classeMusica.py
+++ python/oo-sabores-express/exercicios/classeMusica.py
@@ -1,12 +1,37 @@
+import os
+os.system("cls")
+# class Musica:
+#     nome = ""
+#     artista = ""
+#     duracao = int
 
+# musica1 = Musica()
+# musica1.nome = "Superman"
+# musica1.artista = "Emminem"
+# musica1.duracao = 245
+
+# print(vars(musica1))
+
+"""
+Agora é sua vez! Refaça essa classe Musica utilizando uma forma mais concisa e expressiva, aproveitando a sintaxe simplificada do Python.
+"""
 class Musica:
-    nome = ""
-    artista = ""
-    duracao = int
+    musicas = []
 
-musica1 = Musica()
-musica1.nome = "Superman"
-musica1.artista = "Emminem"
-musica1.duracao = 245
+    def __init__(self, nome, artista, duracao):
+        self.nome = nome
+        self.artista = artista
+        self.duracao = duracao
+        Musica.musicas.append(self)
 
-print(vars(musica1))
+    def __str__(self):
+        return f"{self.nome} | {self.artista} | {self.duracao}"
+    
+    def listar_musicas():
+        for musica in Musica.musicas:
+            print(f"{musica.nome} | {musica.artista} | {musica.duracao}")
+
+musica1 = Musica("Superman", "Eminem", 345)
+musica2 = Musica("Belive", "Imagine Dragrons", 123)
+
+Musica.listar_musicas()
