1f7533a2f1b9e2bc3f6ee473d548f376d094140e
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-06-17 10:19:51 -0300

    metodos da classe

--- python/oo-sabores-express/modelos/restaurante.py
+++ python/oo-sabores-express/modelos/restaurante.py
@@ -21,20 +21,24 @@ class Restaurante:
       """
       return f"{self._nome} | {self._categoria} "
     
-    def listar_restaurantes():
+    @classmethod
+    def listar_restaurantes(cls):
        print(f"{"Nome do restaurante".ljust(25)} | {"Categoria".ljust(25)} | {"Status"}")
-       for restaurante in Restaurante.restaurantes:
-          print(f"{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}")
+       for restaurante in cls.restaurantes:
+          print(f"{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}")
 
     @property # modificar aquele atributo que vai ser lido
     def ativo(self):
        return "⌧" if self._ativo else "☐"
+    
+    def alternar_estado(self):
+       self._ativo = not self._ativo
           
       
     
 
 restaurante_praca = Restaurante("praça", "Gourmet")
-restaurante_praca.ativo = True
+restaurante_praca.alternar_estado()
 restaurante_pizza = Restaurante("pizza express", "Italiana")
 
 Restaurante.listar_restaurantes()
