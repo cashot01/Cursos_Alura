0f9aae6ff25bc29176d9f31f4b26dff9e3eeff53
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-06-21 11:38:52 -0300

    polimorfismo

--- python/oo-sabores-express/modelos/cardapio/item_cardapio.py
+++ python/oo-sabores-express/modelos/cardapio/item_cardapio.py
@@ -8,6 +8,7 @@ class ItemCardapio(ABC):
     @abstractmethod
     #  Isso implica que todas as classes derivadas são obrigadas a seguir essa diretriz; quando dizemos "precisam", é uma exigência real.
     def aplicar_desconto(self):
+        # Essa forma de pegar um método e dizer que ele deve se moldar de forma diferente em diferentes classes é o que chamamos na programação de polimorfismo. O mesmo método se adapta para uma classe específica e tem um comportamento diferente através desse método abstrato que criamos na nossa classe principal.
         pass
 
 
