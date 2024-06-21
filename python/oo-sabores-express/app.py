56154f047e4c13a1e3af773b9a97e5c9602a3390
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-06-21 11:36:03 -0300

    aplicar desconto

--- python/oo-sabores-express/app.py
+++ python/oo-sabores-express/app.py
@@ -4,10 +4,14 @@ from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante("praça", "Gourmet")
 bebida_suco = Bebida("Suco de melância", 5.0, "grande")
+bebida_suco.aplicar_desconto()
+
 prato_paozinho = Prato("Paozinho", 2.00, "Melhor pão da cidade")
+prato_paozinho.aplicar_desconto()
 
 restaurante_praca.adicionar_no_cardapio(bebida_suco)
 restaurante_praca.adicionar_no_cardapio(prato_paozinho)
+
 
 def main():
     restaurante_praca.exibir_cardapio
