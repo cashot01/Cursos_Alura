2a0d9e5d3e6356d71f9e6a77b2a9debf3fd97c15
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-06-21 11:29:22 -0300

    classe abstrata no item_cardaipo

--- python/oo-sabores-express/modelos/cardapio/item_cardapio.py
+++ python/oo-sabores-express/modelos/cardapio/item_cardapio.py
@@ -1,4 +1,14 @@
-class ItemCardapio:
+from abc import ABC, abstractmethod
+class ItemCardapio(ABC):
     def __init__(self, nome, preco):
         self._nome = nome
         self._preco = preco
+    
+
+    @abstractmethod
+    #  Isso implica que todas as classes derivadas são obrigadas a seguir essa diretriz; quando dizemos "precisam", é uma exigência real.
+    def aplicar_desconto(self):
+        pass
+
+
+    
